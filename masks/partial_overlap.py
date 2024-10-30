ea25c79da299413fc050d884605dd5dff3382047
Iain Rolland <imr27@cam.ac.uk>
2024-10-30 15:53:08 +0000

    Updated mask to comply with numpy deprecation of np.bool

--- masks/partial_overlap.py
+++ masks/partial_overlap.py
@@ -8,11 +8,11 @@ def partial_overlap(shape, day_0_remove_n_rows=90, day_0_remove_n_cols=90, day_1
 
     returns a 4th-order tensor (rows, cols, channels, temporal)
     """
-    mask = np.ones(shape + (2,), dtype=np.bool)
+    mask = np.ones(shape + (2,), dtype=bool)
 
-    mask[:day_0_remove_n_rows, ..., 0] = False
-    mask[:, :day_0_remove_n_cols, ..., 0] = False
-    mask[-day_1_remove_n_rows:, ..., 1] = False
-    mask[:, -day_1_remove_n_cols:, ..., 1] = False
+    mask[:day_0_remove_n_rows, ..., 0] = 0
+    mask[:, :day_0_remove_n_cols, ..., 0] = 0
+    mask[-day_1_remove_n_rows:, ..., 1] = 0
+    mask[:, -day_1_remove_n_cols:, ..., 1] = 0
 
     return mask
